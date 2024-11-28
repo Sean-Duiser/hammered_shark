227ee1dcbe9bf2dd58ee64587a5c6f0e24f2ff09
Sean Duiser <duzer84@gmail.com>
2024-11-28 17:36:21 -0500

    logo animation and background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import hs_logo from './img/hs_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* Background GIF */}
+      <div className="background-video-container">
+        <div className="background-video"></div>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Hyperlinked logo */}
+        <a href="https://www.facebook.com/HammeredShark" target="_blank" rel="noopener noreferrer">
+          <img src={hs_logo} className="App-logo" alt="logo" />
+        </a>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/HammeredShark"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow us on Social!
         </a>
+        <a><h2>More Coming Soon...</h2></a>
       </header>
-      <h2>There is life. There is growth. There is connection.</h2>
     </div>
   );
 }
