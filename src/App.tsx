dc9d88c5b06dba9cdee66f27cb5b3a411e2b4f6a
Sean Duiser <duzer84@gmail.com>
2024-11-28 21:10:56 -0500

    using real social link

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import logo from './assets/hs_logo.jpg';
 function App() {
   return (
     <div className="container">
-      <a href="https://www.yoursociallink.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/HammeredShark" target="_blank" rel="noopener noreferrer">
         <img src={logo} className="logo" alt="Hammered Shark Logo" />
       </a>
-      <a href="https://www.yoursociallink.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/HammeredShark" target="_blank" rel="noopener noreferrer">
         Visit Our Social
       </a>
     </div>
